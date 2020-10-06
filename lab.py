33351cb26816537780a3dbb4f004193e12565d47
Artemii Morozov <morozz9846@gmail.com>
2020-10-06 12:08:23 +0300

    Rename image in lab.py

--- lab.py
+++ lab.py
@@ -3,9 +3,9 @@ from madt_lib.network import Network, Overlay
 
 def main():
     net = Network('15.0.0.0/8')
-    node1 = net.create_node('Node0', image='madt/kind', entrypoint="sleep infinity", ports={'8008/tcp': 8008, '9080/tcp': 9080}, enable_internet=True, privileged=True)
-    node2 = net.create_node('Node1', image='madt/kind', entrypoint="sleep infinity", ports={'8009/tcp': 8009}, enable_internet=True, privileged=True)
-    node3 = net.create_node('Node2', image='madt/kind', entrypoint="sleep infinity", ports={'8010/tcp': 8010}, enable_internet=True, privileged=True)
+    node1 = net.create_node('Node0', image='lab/mit', entrypoint="sleep infinity", ports={'8008/tcp': 8008, '9080/tcp': 9080}, enable_internet=True, privileged=True)
+    node2 = net.create_node('Node1', image='lab/mit', entrypoint="sleep infinity", ports={'8009/tcp': 8009}, enable_internet=True, privileged=True)
+    node3 = net.create_node('Node2', image='lab/mit', entrypoint="sleep infinity", ports={'8010/tcp': 8010}, enable_internet=True, privileged=True)
     net.create_subnet('net', (node1, node2, node3))
 
     net.configure(verbose=True)
